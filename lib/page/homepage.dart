5d4199ee3d70b51a6870ee4ee18a9efb022e03b8
narisaraj <120469204+narisaraj@users.noreply.github.com>
2023-01-04 13:45:03 +0700

    update homepage
    use material at homepage for UI

--- lib/page/homepage.dart
+++ lib/page/homepage.dart
@@ -25,124 +25,54 @@ class HomePage extends StatelessWidget {
             SizedBox(
               height: 50,
             ),
-            OutlinedButton(
-              onPressed: () => context.go('/videopage'),
-              child: const Text('Video'),
-              style: OutlinedButton.styleFrom(
-                  minimumSize: Size(150, 50),
-                  textStyle: TextStyle(fontSize: 30, fontFamily: 'ostrich'),
-                  primary: Color.fromARGB(255, 255, 161, 84),
-                  side: BorderSide(
-                      width: 2, color: Color.fromARGB(255, 49, 192, 202))),
+            ListTile(
+              leading: MaterialBtn.primary(context, onPressed: () {
+                context.go('/videopage');
+              }, labelText: LocaleKeys.Video),
+              trailing: MaterialBtn.primary(context, onPressed: () {
+                context.go('/postpage');
+              }, labelText: LocaleKeys.PostPage),
             ),
             SizedBox(
               height: 20,
             ),
-            OutlinedButton(
-              onPressed: () => context.go('/postpage'),
-              child: const Text('Post'),
-              style: OutlinedButton.styleFrom(
-                  minimumSize: Size(150, 50),
-                  textStyle: TextStyle(fontSize: 30, fontFamily: 'ostrich'),
-                  primary: Color.fromARGB(255, 214, 9, 26),
-                  side: BorderSide(
-                      width: 2, color: Color.fromARGB(255, 2, 22, 206))),
+            ListTile(
+              leading: MaterialBtn.primary(context, onPressed: () {
+                context.go('/livenowpage');
+              }, labelText: LocaleKeys.LiveNowPage),
+              trailing: MaterialBtn.primary(
+                context,
+                labelText: LocaleKeys.HomePromo,
+                onPressed: () {
+                  context.go('/homepromo');
+                },
+              ),
             ),
             SizedBox(
               height: 20,
             ),
-            OutlinedButton(
-              onPressed: () => context.go('/livenowpage'),
-              child: const Text('Live Now'),
-              style: OutlinedButton.styleFrom(
-                  minimumSize: Size(150, 50),
-                  textStyle: TextStyle(fontSize: 30, fontFamily: 'ostrich'),
-                  primary: Color.fromARGB(255, 95, 84, 255),
-                  side: BorderSide(
-                      width: 2, color: Color.fromARGB(255, 202, 187, 49))),
+            ListTile(
+              leading: MaterialBtn.primary(
+                context,
+                onPressed: () {
+                  context.go('/setting');
+                },
+                labelText: LocaleKeys.Settings,
+              ),
+              trailing: MaterialBtn.primary(context, onPressed: () {
+                context.go('/help');
+              }, labelText: LocaleKeys.Help),
             ),
             SizedBox(
               height: 20,
             ),
-            // OutlinedButton(
-            //   onPressed: () => context.go('/homepromo'),
-            //   child: const Text('Home Promo'),
-            //   style: OutlinedButton.styleFrom(
-            //       minimumSize: Size(150, 50),
-            //       textStyle: TextStyle(fontSize: 30, fontFamily: 'ostrich'),
-            //       primary: Color.fromARGB(255, 224, 255, 84),
-            //       side: BorderSide(
-            //           width: 2, color: Color.fromARGB(255, 49, 202, 82))),
-            // ),
-            MaterialBtn.primary(
-              context,
-              labelText: LocaleKeys.HomePromo,
-              onPressed: () {
-                context.go('/homepromo');
-              },
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            MaterialBtn.primary(
-              context,
-              labelText: LocaleKeys.Settings,
-              onPressed: () => context.go('/setting'),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            // OutlinedButton(
-            //   onPressed: () => context.go('/help'),
-            //   child: const Text('Help'),
-            //   style: OutlinedButton.styleFrom(
-            //       minimumSize: Size(150, 50),
-            //       textStyle: TextStyle(fontSize: 30, fontFamily: 'ostrich'),
-            //       primary: Colors.white,
-            //       side: BorderSide(
-            //           width: 2, color: Color.fromARGB(255, 27, 255, 217))),
-            // ),
-            MaterialBtn.primary(context,
-                onPressed: () => context.go('/help'),
-                labelText: LocaleKeys.Help),
-            SizedBox(
-              height: 20,
-            ),
-            OutlinedButton(
-              onPressed: () => context.go('/promo'),
-              child: const Text('Promo'),
-              style: OutlinedButton.styleFrom(
-                  minimumSize: Size(150, 50),
-                  textStyle: TextStyle(fontSize: 30, fontFamily: 'ostrich'),
-                  primary: Colors.white,
-                  side: BorderSide(
-                      width: 2, color: Color.fromARGB(255, 27, 255, 217))),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            OutlinedButton(
-              onPressed: () => context.go('/channel'),
-              child: const Text('Channel'),
-              style: OutlinedButton.styleFrom(
-                  minimumSize: Size(150, 50),
-                  textStyle: TextStyle(fontSize: 30, fontFamily: 'ostrich'),
-                  primary: Colors.white,
-                  side: BorderSide(
-                      width: 2, color: Color.fromARGB(255, 27, 255, 217))),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            OutlinedButton(
-              onPressed: () => context.go('/trending'),
-              child: const Text('Trending Tag'),
-              style: OutlinedButton.styleFrom(
-                  minimumSize: Size(150, 50),
-                  textStyle: TextStyle(fontSize: 30, fontFamily: 'ostrich'),
-                  primary: Colors.white,
-                  side: BorderSide(
-                      width: 2, color: Color.fromARGB(255, 27, 255, 217))),
+            ListTile(
+              leading: MaterialBtn.primary(context, onPressed: () {
+                context.go('/channel');
+              }, labelText: LocaleKeys.Channel),
+              trailing: MaterialBtn.primary(context, onPressed: () {
+                context.go('/trending');
+              }, labelText: LocaleKeys.Trending),
             ),
           ],
         ),
